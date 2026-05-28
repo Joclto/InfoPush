994b803290c31b416591676c1f862c8b6daf0fb9
FEITIAN <lft2021@163.com>
2026-05-28 13:09:42 +0800

    chore: 更新配置以忽略额外的环境变量

--- server/app/config.py
+++ server/app/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     host: str = "0.0.0.0"
     port: int = 8000
 
-    model_config = {"env_file": ".env", "env_file_encoding": "utf-8"}
+    model_config = {"env_file": ".env", "env_file_encoding": "utf-8", "extra": "ignore"}
 
 
 def _get_settings() -> Settings:
